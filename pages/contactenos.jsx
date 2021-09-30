959d7e00b3bdf319a843b2506be4c756974b60f2
Jarrison Cano <developeratalaya1@outlook.com>
2021-09-30 15:37:24 -0500

    Agregando las rutas de la pagina

---
+++ pages/contactenos.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const contactenos = () => {
+	return (
+		<div>
+			<h1>contactenos</h1>
+		</div>
+	)
+}
---
+++ pages/quienes-somos/sistema-gestion-integrado.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const sistemaGestionIntegrado = () => {
+	return (
+		<div>
+			<h1>sistema gestion integrado</h1>
+		</div>
+	)
+}
