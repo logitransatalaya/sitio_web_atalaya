4ea12332aaea7043ad8f2518310899b03241b5b9
Jarrison Cano <developeratalaya1@outlook.com>
2021-09-30 17:24:45 -0500

    Agregue el slider de la Home

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'react-bootstrap/dist/react-bootstrap'
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />
