0131866fc804e1659e888f7b9bb150e461a89f83
developer4 <developeratalaya4@gmail.com>
2021-10-02 14:21:45 -0500

    not found ajusted

--- pages/404.jsx
+++ pages/404.jsx
@@ -5,6 +5,11 @@ import styled from '../styles/notfound.module.css'
 const NotFound = () => {
 	return (
 		<div className={`${styled.notFoundC}`}>
+			<div className={`${styled.btn_backCont}`}>
+				<button className={`${styled.btn_back}`}>
+					volver al inicio
+				</button>
+			</div>
 			<NotFoundAnimation />
 		</div>
 	)
