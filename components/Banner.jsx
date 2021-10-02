c8fb334335053174a236d8bb7ab42989923a3287
Camiloatalaya <developeratalaya2@outlook.com>
2021-10-02 10:41:08 -0500

    :zap: add image banner in component privacyPolicies

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import styles from '../styles/banner.module.css'
 
 export const Banner = ({ url }) => {
 	return (
-		<div className=''>
-			<div style={{ width: '100%' }}>
-				<img
-					src={`${url}`}
-					width='100%'
-					height='600px'
-					alt='banners de atalaya'
-				/>
+		<div className={`${styles.container}`}>
+			<div className={`mt-5 mb-5  ${styles.card}`}>
+				<div style={{ width: '100%' }}>
+					<img
+						src={`${url}`}
+						width='100%'
+						height='400rem'
+						alt='banners de atalaya'
+					/>
+				</div>
 			</div>
 		</div>
 	)
