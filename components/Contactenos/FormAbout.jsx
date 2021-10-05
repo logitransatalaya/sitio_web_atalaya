2456c921251494263bb36a9c53724c7789f47fcc
developer4 <developeratalaya4@gmail.com>
2021-10-05 08:05:48 -0500

    final form settings

--- components/Contactenos/FormAbout.jsx
+++ components/Contactenos/FormAbout.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import styled from 'styles/About.module.css'
 import Link from 'next/link'
+import { useEmailTo } from 'Hooks/useEmailTo'
+import Modal from 'components/Modal'
 
 const initialState = {
 	type_client: 'default',
@@ -10,11 +12,25 @@ const initialState = {
 	affair: 'default',
 	comments: ''
 }
+
+const initialStyles = {
+	type_client_S: false,
+	name_S: false,
+	mail_S: false,
+	phone_S: false,
+	affair_S: false,
+	checkInput_S: false
+}
 const FormAbout = () => {
+	const { sendEmail } = useEmailTo()
 	const [valuesInputs, setValuesInputs] = useState(initialState)
+	const [stylesAlerts, setStylesAlerts] = useState(initialStyles)
 	const [checkInput, setCheckInput] = useState(false)
+	const [showModal, setShowModal] = useState(false)
 
 	const { type_client, name, mail, phone, affair, comments } = valuesInputs
+	const { type_client_S, name_S, mail_S, phone_S, affair_S, checkInput_S } =
+		stylesAlerts
 
 	const handleInputs = (e) => {
 		setValuesInputs({
@@ -40,17 +56,101 @@ const FormAbout = () => {
 	}
 
 	const handleSubmit = (e) => {
+		e.stopPropagation()
 		e.preventDefault()
 
+		if (type_client === 'default') {
+			setStylesAlerts({
+				...stylesAlerts,
+				type_client_S: true
+			})
+
+			return
+		}
+		if (name === '') {
+			setStylesAlerts({
+				...stylesAlerts,
+				name_S: true
+			})
+			return
+		} else if (name.length < 4) {
+			setStylesAlerts({
+				...stylesAlerts,
+				name_S: true
+			})
+		}
+		if (mail === '') {
+			setStylesAlerts({
+				...stylesAlerts,
+				mail_S: true
+			})
+			return
+		}
+		if (typeof mail !== 'undefined') {
+			let posicionArroba = mail.lastIndexOf('@')
+			let posicionPunto = mail.lastIndexOf('.')
+
+			if (
+				!(
+					posicionArroba < posicionPunto &&
+					posicionArroba > 0 &&
+					mail.indexOf('@@') == -1 &&
+					posicionPunto > 2 &&
+					mail.length - posicionPunto > 2
+				)
+			) {
+				setStylesAlerts({
+					...stylesAlerts,
+					mail_S: true
+				})
+				return
+			}
+		}
+		if (phone === '') {
+			setStylesAlerts({
+				...stylesAlerts,
+				phone_S: true
+			})
+			return
+		}
+		if (affair === 'default') {
+			setStylesAlerts({
+				...stylesAlerts,
+				affair_S: true
+			})
+			return
+		}
 		if (!checkInput) {
-			return alert('Tienes que capetar las politicas')
+			setStylesAlerts({
+				...stylesAlerts,
+				checkInput_S: true
+			})
+			return
 		}
 
-		alert('Se esta enviando')
+		sendEmail(valuesInputs)
+		setShowModal(true)
+		setValuesInputs(initialState)
+		setCheckInput(false)
+		setStylesAlerts({
+			type_client_S: false,
+			name_S: false,
+			mail_S: false,
+			phone_S: false,
+			affair_S: false,
+			checkInput_S: false
+		})
+		setTimeout(() => {
+			setShowModal(false)
+		}, 3000)
+	}
+
+	const handleModal = () => {
+		setShowModal(!showModal)
 	}
 
 	return (
-		<form className={styled.form} onSubmit={handleSubmit}>
+		<form className={styled.form}>
 			<h2 className={styled.form_title}>
 				Déjenos conocer su requerimiento y pronto lo contactaremos
 			</h2>
@@ -71,6 +171,16 @@ const FormAbout = () => {
 						Tipo de cliente:
 					</label>
 					<span className={styled.form_line}></span>
+					<span
+						id='tipoCliente'
+						className={`${
+							type_client_S
+								? `${styled.alert} ${styled.visible} ${styled.ajuste}`
+								: `${styled.alert}`
+						}`}
+					>
+						Debe ingresar un tipo de cliente
+					</span>
 				</div>
 				<div className={styled.form_group}>
 					<input
@@ -86,6 +196,15 @@ const FormAbout = () => {
 						Nombre:
 					</label>
 					<span className={styled.form_line}></span>
+					<span
+						className={`${
+							name_S
+								? `${styled.alert} ${styled.visible}`
+								: `${styled.alert}`
+						}`}
+					>
+						Debe ingresar un Nombre
+					</span>
 				</div>
 				<div className={styled.form_group}>
 					<input
@@ -101,6 +220,15 @@ const FormAbout = () => {
 						E-mail:
 					</label>
 					<span className={styled.form_line}></span>
+					<span
+						className={`${
+							mail_S
+								? `${styled.alert} ${styled.visible}`
+								: `${styled.alert}`
+						}`}
+					>
+						Debe ingresar un correo o un correo valido
+					</span>
 				</div>
 				<div className={styled.form_group}>
 					<input
@@ -112,10 +240,23 @@ const FormAbout = () => {
 						value={phone}
 						onChange={handleInputs}
 					/>
-					<label htmlFor='phone' className={styled.form_label}>
+					<label
+						htmlFor='phone'
+						className={styled.form_label}
+						type='tel'
+					>
 						Celular:
 					</label>
 					<span className={styled.form_line}></span>
+					<span
+						className={`${
+							phone_S
+								? `${styled.alert} ${styled.visible}`
+								: `${styled.alert}`
+						}`}
+					>
+						Debe ingresar un numero de contacto
+					</span>
 				</div>
 				<div className={styled.form_group}>
 					<select
@@ -138,6 +279,15 @@ const FormAbout = () => {
 						Asunto:
 					</label>
 					<span className={styled.form_line}></span>
+					<span
+						className={`${
+							affair_S
+								? `${styled.alert} ${styled.visible}`
+								: `${styled.alert}`
+						}`}
+					>
+						Debe seleccionar un asunto
+					</span>
 				</div>
 				<div className={styled.form_group}>
 					<textarea
@@ -162,6 +312,7 @@ const FormAbout = () => {
 						type='checkbox'
 						name={'politics'}
 						onChange={() => setCheckInput(!checkInput)}
+						checked={checkInput}
 					/>
 					<label htmlFor='check'>
 						Estoy de acuerdo con las políticas de tratamiento de
@@ -171,10 +322,30 @@ const FormAbout = () => {
 						</Link>
 					</label>
 				</div>
+
+				<span
+					className={`${
+						checkInput_S
+							? `${styled.alert} ${styled.visible}`
+							: `${styled.alert}`
+					}`}
+				>
+					Debe aceptar las politicas de tratamiento de datos
+				</span>
 				<div className={styled.btn_container}>
-					<button className={styled.btn_submit}>Enviar</button>
+					<button
+						className={styled.btn_submit}
+						onClick={handleSubmit}
+					>
+						Enviar
+					</button>
 				</div>
 			</div>
+			{showModal && (
+				<Modal handleModal={handleModal}>
+					<h1>Información enviada satisfactoriamente</h1>
+				</Modal>
+			)}
 		</form>
 	)
 }
