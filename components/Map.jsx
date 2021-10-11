2f7c8fbf84fc29c345d65673fe99fc6d2bb6a93e
Developer 1 <developeratalaya1@outlook.com>
2021-10-11 10:09:13 -0500

    change google maps ubication

--- components/Map.jsx
+++ components/Map.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Map = () => {
 	return (
 		<iframe
-			src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7848.67244229742!2d-75.521779!3d10.394848!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef62580ed2c0c23%3A0x5f4623e38ce07343!2shotel%20bosque%20real!5e0!3m2!1ses!2sco!4v1633179850856!5m2!1ses!2sco'
+			src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d294.7489884883296!2d-75.55538128533765!3d6.3091206355478215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e442f70cbdad5d9%3A0xd29b057b214bbec6!2sHOTEL%20LAS%20OLAS!5e0!3m2!1ses!2sco!4v1633964860385!5m2!1ses!2sco'
 			allowFullScreen=''
 			loading='lazy'
 			style={{
