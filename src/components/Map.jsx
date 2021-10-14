ab1f4dcef9b79fa822bb5ca031a8fb982b0b9500
Jarrison <89084344+JarrisonAtalaya@users.noreply.github.com>
2021-10-14 16:53:55 -0500

    drop uneccesary files

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,7 +4,7 @@ export const Map = () => {
 	return (
 		<iframe
 			title='google maps'
-			src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d294.7489884883296!2d-75.55538128533765!3d6.3091206355478215!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e442f70cbdad5d9%3A0xd29b057b214bbec6!2sHOTEL%20LAS%20OLAS!5e0!3m2!1ses!2sco!4v1633964860385!5m2!1ses!2sco'
+			src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d495.7061991190993!2d-75.5552348!3d6.3096815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e442f6903756613%3A0x9aaf72ccb3fe432b!2sLog%C3%ADstica%20y%20Transporte%20Atalaya!5e0!3m2!1ses-419!2sco!4v1634247941169!5m2!1ses-419!2sco'
 			allowFullScreen=''
 			loading='lazy'
 			style={{
