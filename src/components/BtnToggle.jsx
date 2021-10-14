d73e9763a827e1e39d5111aa6500f38bb84243dd
Jarrison <89084344+JarrisonAtalaya@users.noreply.github.com>
2021-10-14 17:27:49 -0500

    drop console.log

--- src/components/BtnToggle.jsx
+++ src/components/BtnToggle.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from 'styles/BtnToggle.module.css'
 
 export const BtnToggle = ({ state }) => {
-	console.log('entra')
 	const styleToggle = state
 		? styles.toggle
 		: `${styles.toggle} ${styles.active}`
