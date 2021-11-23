5464e3ed32f0eca2fd34aa2aa8308a3ea5c884c4
Developer 1 <developeratalaya1@outlook.com>
2021-11-23 09:51:53 -0500

    change code for init server

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.get('/ping', function (req, res) {
-	return res.send('pong')
-})
+app.use(express.static(path.join(__dirname)))
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'))
+	res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 3000)
